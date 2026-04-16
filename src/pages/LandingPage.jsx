4fd66412b4b4f6bdb1e878e6a2e1d1e5af6be329
ryhndastra <ryhndastra@gmail.com>
2026-04-16 23:55:05 +0700

    .

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,10 +4,12 @@ import Features from "../components/Features";
 import HowItWorks from "../components/HowItWorks";
 import LandingCTA from "../components/LandingCTA";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function LandingPage() {
   return (
     <div className="min-h-screen">
+      <Navbar variant="Landing" />
       <LandingHero />
       <Features />
       <HowItWorks />
